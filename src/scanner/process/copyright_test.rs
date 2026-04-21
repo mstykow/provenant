72d7c5e75473e4438922c55ef47a4178fd7134b5
Maxim Stykow <maxim.stykow@gmail.com>
2026-04-21 12:17:41 +0200

    fix(copyright): recover comment and patch authors
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/scanner/process/copyright_test.rs
+++ src/scanner/process/copyright_test.rs
@@ -1,4 +1,7 @@
-use super::is_binary_string_copyright_candidate;
+use super::{
+    extract_comment_author_supplements, extract_patch_header_author_supplements,
+    is_binary_string_copyright_candidate,
+};
 
 #[test]
 fn test_binary_string_copyright_candidate_rejects_gibberish_holder_text() {
@@ -17,4 +20,42 @@ fn test_binary_string_copyright_candidate_rejects_changelog_phrase() {
     assert!(!is_binary_string_copyright_candidate(
         "Copyright - split out libs"
     ));
+}
+
+#[test]
+fn test_extract_patch_header_author_supplements_collects_common_patch_headers() {
+    let text = "From: Robert Scheck <robert@fedoraproject.org>\n\
+Signed-off-by: Khem Raj <raj.khem@gmail.com>\n\
+Patch by Example Person <example@example.com>\n";
+
+    let authors = extract_patch_header_author_supplements(text);
+    let values: Vec<_> = authors.into_iter().map(|author| author.author).collect();
+
+    assert_eq!(
+        values,
+        vec![
+            "Robert Scheck <robert@fedoraproject.org>",
+            "Khem Raj <raj.khem@gmail.com>",
+            "Example Person <example@example.com>",
+        ]
+    );
+}
+
+#[test]
+fn test_extract_comment_author_supplements_collects_written_by_and_email_name_forms() {
+    let text = "# udhcpc script edited by Tim Riker <Tim@Rikers.org>\n\
+#   clst@ambu.com (Claus Stovgaard)\n\
+#                by Ian Murdock <imurdock@gnu.ai.mit.edu>.\n";
+
+    let authors = extract_comment_author_supplements(text);
+    let values: Vec<_> = authors.into_iter().map(|author| author.author).collect();
+
+    assert_eq!(
+        values,
+        vec![
+            "Tim Riker <Tim@Rikers.org>",
+            "Claus Stovgaard <clst@ambu.com>",
+            "Ian Murdock <imurdock@gnu.ai.mit.edu>",
+        ]
+    );
 }
